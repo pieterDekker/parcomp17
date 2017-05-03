475b61bcbf40caf81ec5e97c9adcc4e3cd6466b4
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2017-05-03 11:44:00 +0200

    Parallelized Pi.c, automated I/O

--- week3/Code/Pi/pi.c
+++ week3/Code/Pi/pi.c
@@ -2,61 +2,52 @@
 #include <stdio.h>
 #include <math.h>
 #include <sys/time.h>
+#include <omp.h>
 
 #define f(x) (4.0/(1.0+(x)*(x)))
 
-static double timer(void)
-{
-  struct timeval tm;
-  gettimeofday (&tm, NULL);
-  return tm.tv_sec + tm.tv_usec/1000000.0;
+// 3.1415926535897932384
+static double timer(void) {
+    struct timeval tm;
+    gettimeofday(&tm, NULL);
+    return tm.tv_sec + tm.tv_usec / 1000000.0;
 }
 
-int main(int argc, char **argv)
-{
-  int i, N;
-  double dx, sum, err;
-  double clock;
+int main(int argc, char **argv) {
+    int N;
+    double dx, sum, err;
+    double clock;
+    long approxArray[] = {1000,1000000,10000000000};
+    int threadsArray[] = {1,4,8,12};
 
-  while (1)
-  {
-    /* Step (1): get a value for N */
-    do {
-      printf ("Enter number of approximation intervals:(0 to exit)\n");
-      scanf ("%d", &N);
-    } while (N<0);
-    printf ("N=%d\n", N);
+    for(int i = 0; i < 3; ++i) {
+        for(int j = 0; j < 4; ++j) {
+            printf("Number of approximations: %ld\nNumber of threads: %d\n",approxArray[i],threadsArray[j]);
+            omp_set_num_threads(threadsArray[j]);
+            N = approxArray[i];
+            /* start timer */
+            clock = timer();
+            /* Step (3): numerical integration */
+            dx = 1.0 / N;
+            sum = 0.0;
+            #pragma omp parallel for reduction(+:sum)
+            for (i = 0; i < N; i++) {
+                sum = sum + f(dx * (i + 0.5));
+            }
+            sum *= dx;
 
-    /* Step (2): check for exit condition. */
-    if (N == 0)
-    {
-      printf("Done\n");
-      break;
+            /* stop timer */
+            clock = timer() - clock;
+            printf("wallclock: %lf seconds\n", clock);
+
+            /* Step (4): print the results */
+            err = sum - 4 * atan(1);
+            printf("%d: pi~%20.18lf   er~=%20.18lf\n", N, sum, err);
+        }
     }
 
-    /* start timer */
-    clock = timer();
-     
-    /* Step (3): numerical integration */
-    dx = 1.0/N;
-    sum = 0.0;
-    for (i=0; i < N; i++)
-    {
-      sum = sum + f(dx*(i+0.5));
-    }
-    sum *= dx;
-
-    /* stop timer */
-    clock = timer() - clock;
-    printf ("wallclock: %lf seconds\n", clock);
-
-    /* Step (4): print the results */
-    err = sum - 4*atan(1);
-    printf ("%d: pi~%20.18lf   er~=%20.18lf\n", N, sum, err);
-  }
-  
-  printf("\n");
-  system("echo $USER");
-  system("date");
-  return EXIT_SUCCESS;
+    printf("\n");
+    system("echo $USER");
+    system("date");
+    return EXIT_SUCCESS;
 }
