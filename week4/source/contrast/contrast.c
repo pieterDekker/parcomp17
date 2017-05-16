bb18e4fad78f0fad063f523c3de30af83c9ccc2a
pieterDekker <pieter.dekker20@gmail.com>
2017-05-16 22:11:57 +0200

    incremental change

--- week4/source/contrast/contrast.c
+++ week4/source/contrast/contrast.c
@@ -12,20 +12,20 @@
 #define MASTER 0
 #define SINGLE 1
 
-void error(char *errmsg) {
-    fputs(errmsg, stderr);
-    exit(EXIT_FAILURE);
+void error (char *errmsg) {
+    fputs (errmsg,stderr);
+    exit (EXIT_FAILURE);
 }
 
-void *safeMalloc(int n) {
-    void *ptr = malloc(n);
+void *safeMalloc (int n) {
+    void *ptr = malloc (n);
     if (ptr == NULL) {
-        error("Error: memory allocation failed.\n");
+        error ("Error: memory allocation failed.\n");
     }
     return ptr;
 }
 
-// Stretches the contrast of an image. 
+// Stretches the contrast of an image.
 void contrastStretch(int low, int high, Image image) {
     int row, col, min, max;
     int width = image->width, height = image->height, **im = image->imdata;
@@ -52,7 +52,7 @@ void contrastStretch(int low, int high, Image image) {
     max = absMax;
 
     //Barrier
-    
+
     // Compute scale factor.
     scale = (float) (high - low) / (max - min);
 
@@ -66,12 +66,12 @@ void contrastStretch(int low, int high, Image image) {
 
 }
 
-Image sendChunks(Image image, int numtasks, int chunkSize, int *sendCounts, int *displs) {
+Image sendChunks(Image image, int numtasks,int chunkSize) {
     MPI_Bcast(&chunkSize, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
-    int *buf = safeMalloc(chunkSize * sizeof(int));
+    int *buf = safeMalloc(chunkSize*sizeof(int));
 
-    MPI_Scatterv(image->imdata[0], chunkSize, MPI_INT, buf,
-                 chunkSize, MPI_INT, MASTER, MPI_COMM_WORLD);
+    MPI_Scatter(image->imdata[0], chunkSize, MPI_INT, buf,
+                chunkSize, MPI_INT, MASTER, MPI_COMM_WORLD);
 
     Image workingImage;
     workingImage->width = chunkSize;
@@ -80,7 +80,7 @@ Image sendChunks(Image image, int numtasks, int chunkSize, int *sendCounts, int 
     return workingImage;
 }
 
-Image collectChunks() {
+Image collectChunks(){
 
 }
 
@@ -100,9 +100,6 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    int *sendCount;
-    int *displs;
-
     if (rank == MASTER) {
         // Prints current date and user. DO NOT MODIFY
         system("date");
@@ -110,19 +107,9 @@ int main(int argc, char **argv) {
         //Only the master thread executes this
         image = readImage(argv[1]);
         chunkSize = (image->height / numtasks) * image->width;
-
-        *sendCounts = safeMalloc(numtasks * sizeof(int));
-        *displs = safeMalloc(numtasks * sizeof(int));
-
-        for (int i = 0; i < numtasks - 1; i++) {
-            sendCounts[i] = chunkSize;
-            displs[i] = i * chunkSize;
-        }
-        sendCount[numtasks - 1] = image->height * image->width - (numtasks - 1) * chunkSize;
-        displs[numtasks - 1] = (numtasks - 1) * chunkSize;
     }
 
-    Image workingChunk = sendChunks(image, numtasks, chunkSize, sendCount, displs);
+    Image workingChunk = sendChunks(image, numtasks, chunkSize);
 
     // Do work
     //contrastStretch();
